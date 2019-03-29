cd6c52fbd4e57433f99875c30f8349dc258395e2
Ubuntu <radius@hgrradiusserver.localdomain>
2019-03-29 06:33:00 +0000

    Change logo and wording

--- daloradius-users/include/menu/menu-items.php
+++ daloradius-users/include/menu/menu-items.php
@@ -1,10 +1,10 @@
                 <div id="header">
 
-                                <form action="">
+                                <!--<form action="">
                                 <input value="Search" />
-                                </form>
+                                </form>-->
 
-                                <h1><a href="index.php"> <img src="images/daloradius_small.png" border=0/></a></h1>
+                                <h1><a href="index.php"> <img src="images/hgr_logo.png" border=0/></a></h1>
 
                                 <h2>
                                 
@@ -18,7 +18,7 @@
 				<li><a href="index.php" <?php echo ($m_active == "Home") ? "class=\"active\"" : ""?>><?php echo t('menu','Home'); ?></a></li>
 				<li><a href="pref-main.php" <?php echo ($m_active == "Preferences") ? "class=\"active\"" : ""?>><?php echo t('menu','Preferences'); ?></li>
 				<li><a href="acct-main.php" <?php echo ($m_active == "Accounting") ? "class=\"active\"" : "" ?>><?php echo t('menu','Accounting'); ?></a></li>
-				<li><a href="billing-main.php" <?php echo ($m_active == "Billing") ? "class=\"active\"" : "" ?>><?php echo t('menu','Billing'); ?></a></li>
+				<!--<li><a href="billing-main.php" <?php echo ($m_active == "Billing") ? "class=\"active\"" : "" ?>><?php echo t('menu','Billing'); ?></a></li>-->
 				<li><a href="graph-main.php" <?php echo ($m_active == "Graphs") ? "class=\"active\"" : ""?>><?php echo t('menu','Graphs'); ?></li>
 
                                 </ul>
